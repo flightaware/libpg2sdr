be9fadeb5a6904b81b7617c1a94cbe7d6836225c
Oliver Jowett <oliver.jowett@flightaware.com>
2025-08-22 09:08:04 +0800

    Remove DFU-related stuff from internal.h which doesn't need to be there

--- include/internal.h
+++ include/internal.h
@@ -136,30 +136,8 @@ struct lpcsdr_device_handle {
     unsigned int registers_count;
 };
 
-enum dfu_error {
-    DFU_ERROR_TARGET = -200, // File is not targeted for use by this device.
-    DFU_ERROR_FILE = -201, // File is for this device but fails some vendor-specific verification test.
-    DFU_ERROR_WRITE = -202, // Device is unable to write memory.
-    DFU_ERROR_ERASE = -203, // Memory erase function failed.
-    DFU_ERROR_CHECK_ERASED = -204, // Memory erase check failed.
-    DFU_ERROR_PROG = -205, // Program memory function failed.
-    DFU_ERROR_VERIFY = -206, // Programmed memory failed verification.
-    DFU_ERROR_ADDRESS = -207, // Cannot program memory due to received address that is out of range.
-    DFU_ERROR_NOTDONE = -208, // Received DFU_DNLOAD with wLength = 0, but device does not think it has all of the data yet.
-    DFU_ERROR_FIRMWARE = -209, // Device’s firmware is corrupt. It cannot return to run-time (non-DFU) operations.
-    DFU_ERROR_VENDOR = -210, // iString indicates a vendor-specific ERROR_or.
-    DFU_ERROR_USBR = -211, // Device detected unexpected USB reset signaling.
-    DFU_ERROR_POR = -212, // Device detected unexpected power on reset.
-    DFU_ERROR_UNKNOWN = -213,
-    DFU_ERROR_STALLEDPKT  = -214, // Device stalled an unexpected request.
-    DFU_ERROR_NON_IDLE_STATE = -215, // During DFU download after GET_STATUS request, state is NOT in dfuDNLOAD-IDLE  state.
-};
-
-int dfu_download_firmware(libusb_device_handle *handle, int block , u_int32_t *buffer, int count);
-int dfu_get_status(libusb_device_handle *dev, dfu_status **status);
 int lpcsdr_upload_firmware(lpcsdr_context *ctx, libusb_device_handle *handle);
 int lpcsdr_handle_rom_bootloader(lpcsdr_context *ctx, libusb_device *original_dev, libusb_device **reenumerated_dev);
-int translate_dfu_status(int dfu_status);
 
 int lpcsdr_translate_libusb_error(struct lpcsdr_context *ctx, int error);
 int lpcsdr_translate_errno(lpcsdr_context *ctx, int error);
