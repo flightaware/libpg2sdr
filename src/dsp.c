4362a67fa5df9c9417614a9c59425bd9b9d549a5
Oliver Jowett <oliver.jowett@flightaware.com>
2025-09-04 17:27:25 +0800

    fs4_mix: properly initialize all the output
    This is a stupid bug where it wasn't zeroing out the unused real/imag
    parts of the output. It would "usually" work, if the ADC buffer happened
    to be zeroed out (we malloc it, so it's unpredictable), but it would
    often fail after changing the sampling rate because the new buffer would
    reuse some old memory and have garbage, not zeros.
    
    Manifested as extra noise, anywhere from "why do I get periodic bursts
    with a little extra background noise" to "input is nothing but noise"

--- src/dsp.c
+++ src/dsp.c
@@ -97,8 +97,12 @@ static uint32_t fs4_mix(const int16_t *in, uint32_t in_length, cs16_t *out)
     assert (in_length % 4 == 0);
     for (uint32_t i = 0; i < in_length; i += 4, in += 4, out += 4) {
         out[0].i = in[0];
+        out[0].q = 0;
+        out[1].i = 0;
         out[1].q = -in[1];
         out[2].i = -in[2];
+        out[2].q = 0;
+        out[3].i = 0;
         out[3].q = in[3];
     }
     return in_length;
