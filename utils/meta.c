70edb3f3fdcb5b9e9aa237ccd041d8896b2092db
Oliver Jowett <oliver.jowett@flightaware.com>
2026-02-10 20:39:21 +0800

    Add meta_query, meta_free for querying image / active firmware metadata

---
+++ utils/meta.c
@@ -0,0 +1,96 @@
+#include "meta.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+#include "log.h"
+#include "device.h"
+
+/* retrieve firmware metadata from active firmware */
+static int ctrl_get_metadata(libusb_device_handle *handle, firmware_metadata_t *metadata)
+{
+    int rc = libusb_control_transfer(handle,
+                                     LIBUSB_REQUEST_TYPE_VENDOR | LIBUSB_RECIPIENT_DEVICE | LIBUSB_ENDPOINT_IN,
+                                     EP0_IN_METADATA,
+                                     0, /* wValue */
+                                     0, /* wIndex */
+                                     (unsigned char *)metadata,
+                                     sizeof(*metadata),
+                                     1000); /* timeout */
+    if (rc < 0)
+        return rc;
+
+    metadata->version = le32toh(metadata->version);
+    metadata->compat = le32toh(metadata->compat);
+    metadata->max_control_transfer = le16toh(metadata->max_control_transfer);
+    metadata->control_timeout_ms = le16toh(metadata->control_timeout_ms);
+    metadata->build_type[sizeof(metadata->build_type)-1] = 0;
+
+    return 0;
+}
+
+static void populate_active_meta(libusb_device *dev, port_metadata_t *meta)
+{
+    meta->active_firmware_valid = false;
+
+    libusb_device_handle *handle = device_open(dev, true);
+    if (!handle)
+        return;
+
+    int usb_error;
+    if ((usb_error = ctrl_get_metadata(handle, &meta->active_firmware)) < 0) {
+        log_perror_libusb(usb_error, "fetching firmware metadata failed");
+        return;
+    }
+
+    meta->active_firmware_valid = true;
+}
+
+static void populate_flash_meta(libusb_device *dev, port_metadata_t *meta)
+{
+    firmware_io_t *io = io_open_flash(dev, /* readonly */ true);
+    if (!io)
+        return;
+
+    meta->flash_image = image_read(io);
+    io->close(io);
+}
+
+port_metadata_t *meta_query(libusb_device *dev)
+{
+    port_metadata_t *meta;
+
+    if (!(meta = calloc(1, sizeof(*meta)))) {
+        log_perror("calloc");
+        return NULL;
+    }
+
+    meta->port = strdup(device_ports(dev));
+
+    if (device_is_dfu(dev)) {
+        meta->device_type = DEVICE_DFU;
+    } else if (device_is_pg2(dev)) {
+        meta->device_type = DEVICE_PG2;
+
+        const char *serial = device_serial(dev);
+        if (serial)
+            meta->serial = strdup(serial);
+
+        populate_active_meta(dev, meta);
+        populate_flash_meta(dev, meta);
+    } else {
+        meta->device_type = DEVICE_OTHER;
+    }
+
+    return meta;
+}
+
+void meta_free(port_metadata_t *meta)
+{
+    if (!meta)
+        return;
+
+    free((char*)meta->port);
+    free((char*)meta->serial);
+    image_free(meta->flash_image);
+}
---
+++ utils/meta.h
@@ -0,0 +1,27 @@
+#ifndef PG2_META_H
+#define PG2_META_H
+
+#include "pg2sdr_protocol.h"
+#include "image.h"
+
+typedef enum {
+    DEVICE_OTHER,  /* non-PG2 device connected */
+    DEVICE_DFU,    /* PG2 or other LPC device in DFU mode */
+    DEVICE_PG2     /* PG2 with loaded firmware */
+} device_type_t;
+
+typedef struct {
+    const char *port;   /* strdup'd bus-port[.port[.port ...]] */
+    device_type_t device_type;
+    const char *serial; /* strdup'd PG2 serial number, or NULL if unavailable */
+
+    bool active_firmware_valid;
+    firmware_metadata_t active_firmware;
+
+    firmware_image_t *flash_image;
+} port_metadata_t;
+
+port_metadata_t *meta_query(libusb_device *dev);
+void meta_free(port_metadata_t *meta);
+
+#endif
