3e68595780304cfbe94ea96dca2b0640d5023af7
Oliver Jowett <oliver.jowett@flightaware.com>
2026-02-19 10:50:50 +0800

    pg2-firmware device-info: don't complain about empty flash
    Avoid calling image_read if device flash appears to be empty; otherwise
    we get a confusing error from the image-reading sanity checks.

--- utils/meta.c
+++ utils/meta.c
@@ -46,13 +46,35 @@ static void populate_active_meta(libusb_device *dev, port_metadata_t *meta)
     meta->active_firmware_valid = true;
 }
 
+/* return true if it looks like the data accessed by 'io' is empty
+ * (erased) flash -- i.e. if the first flash page is all 0xFF.
+ */
+static bool is_empty_flash(firmware_io_t *io)
+{
+    uint8_t page[256];
+
+    if (!io->read(io, 0, page, sizeof(page)))
+        return false;
+
+    for (size_t i = 0; i < sizeof(page); ++i)
+        if (page[i] != 0xFF)
+            return false;
+
+    return true;
+}
+
 static void populate_flash_meta(libusb_device *dev, port_metadata_t *meta)
 {
     firmware_io_t *io = io_open_flash(dev, /* readonly */ true);
     if (!io)
         return;
 
-    meta->flash_image = image_read(io);
+    /* If flash is entirely empty, avoid the call to image_read as it
+     * will complain if asked to read empty flash
+     */
+    if (!is_empty_flash(io))
+        meta->flash_image = image_read(io);
+
     io->close(io);
 }
 
